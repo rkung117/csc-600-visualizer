0349ac31e5b2fa60cb7961aba357bb5c7d772126
Rollin Kung <rkung117@gmail.com>
2022-04-11 13:33:50 -0700

    added violin to list of instruments

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -3,6 +3,7 @@ import { List, Map } from 'immutable';
 
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
+import { ViolinInstrument } from './instruments/rkung117-Violin';
 import { WaveformVisualizer } from './visualizers/Waveform';
 
 
@@ -15,7 +16,7 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, ViolinInstrument]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
