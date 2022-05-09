2181a262370af426f9ea6014a977ff86d053962a
Rollin Kung <rkung117@gmail.com>
2022-05-09 15:45:40 -0700

    changed some visualizer code

--- client/src/visualizers/rkung117.tsx
+++ client/src/visualizers/rkung117.tsx
@@ -1,4 +1,5 @@
 // 3rd party library imports
+import { CircleDash16 } from '@carbon/icons-react';
 import P5 from 'p5';
 import * as Tone from 'tone';
 
@@ -13,7 +14,7 @@ export const ColorVisualizer = new Visualizer(
     const height = window.innerHeight / 2;
     const dim = Math.min(width, height);
 
-    p5.background(0, 0, 0, 255);
+    p5.background(0, 150, 150, 255);
 
     p5.strokeWeight(dim * 0.01);
     p5.stroke(255, 255, 255, 255);
@@ -22,11 +23,13 @@ export const ColorVisualizer = new Visualizer(
     const values = analyzer.getValue();
     p5.beginShape();
     for (let i = 0; i < values.length; i++) {
+      // const amplitude = values[i] as number;
+      // const x = p5.map(i, 0, values.length - 1, 0, width);
+      // const y = height / 2 + amplitude * height;
+      // // Place vertex
+      // p5.vertex(x, y);
       const amplitude = values[i] as number;
-      const x = p5.map(i, 0, values.length - 1, 0, width);
-      const y = height / 2 + amplitude * height;
-      // Place vertex
-      p5.vertex(x, y);
+      p5.circle(width / 2 + amplitude * height, height / 2 + amplitude * height, 100);
     }
     p5.endShape();
   },
